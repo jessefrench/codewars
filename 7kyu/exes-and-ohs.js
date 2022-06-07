9df5520bedd5818f90d973d52c6b5eb176ea357e
Jesse French <frejes32@gmail.com>
2022-06-07 18:52:20 -0500

    Add kata

---
+++ 7kyu/exes-and-ohs.js
@@ -0,0 +1,14 @@
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any character.
+
+function XO(str){
+    let x = 0
+    let o = 0
+    for (let i = 0; i < str.length; i++){
+        if (str[i].toLowerCase() === 'x'){
+            x++
+        }else if (str[i].toLowerCase() === 'o'){
+            o++
+        }
+    }
+    return x === o
+}
