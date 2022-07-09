588c99e6db11204f31ca57271f4627a10b41b08a
Jesse French <frejes32@gmail.com>
2022-07-08 19:58:05 -0500

    Add kata

---
+++ 7kyu/isograms.js
@@ -0,0 +1,11 @@
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+function isIsogram(str){
+    const arr = str.toLowerCase().split('').sort()
+    for (let i = 0; i < arr.length - 1; i++){
+      if (arr[i] === arr[i + 1]){
+        return false
+      }
+    }
+    return true
+}
