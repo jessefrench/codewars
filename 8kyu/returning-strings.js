44afdbbe8985b537b3c2916bb85ac84cdfd09961
Jesse French <frejes32@gmail.com>
2022-07-26 09:14:00 -0500

    Add kata

---
+++ 8kyu/returning-strings.js
@@ -0,0 +1,9 @@
+// Parameters: a name as a string
+// Returns: a greeting that includes an input for a name
+// Examples: "Hello, <name> how are you doing today?"
+// Pseudo code: Function 'greet' passes in a string 'name'. The function returns a greeting and uses a template literal to get the name out of the input.
+
+// Time to solve:
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
+}
