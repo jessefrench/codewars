9acf19f13d3e7375051508b8e8de58a95098eeb3
Jesse French <frejes32@gmail.com>
2022-08-23 23:19:12 -0500

    add kata

---
+++ 8kyu/reversed-sequence.js
@@ -0,0 +1,16 @@
+// Reversed Sequence
+
+// DESCRIPTION:
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// EXAMPLE:
+// n=5 --> [5,4,3,2,1]
+
+// SOLUTION:
+const reverseSeq = n => {
+    let result = []
+    for(let i = n; i > 0; i--){
+      result.push(i)
+    }
+    return result
+}
