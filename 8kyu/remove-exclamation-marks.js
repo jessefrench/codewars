c336ae667dfa8bd1b0490957de091c2ae94e7beb
Jesse French <frejes32@gmail.com>
2022-10-01 12:00:00 -0500

    add kata

---
+++ 8kyu/remove-exclamation-marks.js
@@ -0,0 +1,9 @@
+// Remove Exclamation Marks
+
+// DESCRIPTION:
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+// SOLUTION:
+function removeExclamationMarks(s){
+    return s.split('!').join('')
+}
