f491c081fa54aa2c8e5abc41e36633678a484e1e
Jesse French <frejes32@gmail.com>
2022-11-11 12:00:00 -0600

    add kata

---
+++ 8kyu/function-2-squaring-an-argument.js
@@ -0,0 +1,9 @@
+// Function 2 - Squaring an Argument
+
+// DESCRIPTION:
+// Write a function that takes an argument and returns the square of it.
+
+// SOLUTION:
+function square(x){
+    return x ** 2
+}
