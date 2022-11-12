ee97e01f872d3445a0f6409d3624b567cd1c5151
Jesse French <frejes32@gmail.com>
2022-11-12 12:00:00 -0600

    add kata

---
+++ 8kyu/basic-variable-assignment.js
@@ -0,0 +1,9 @@
+// Basic Variable Assignment
+
+// DESCRIPTION:
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+// SOLUTION:
+let a = "code"
+let b = "wa.rs"
+let name = a + b
