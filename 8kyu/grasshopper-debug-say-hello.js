a0e837b83d3b2eba4a3aa4594a7c6f6b6b2df312
Jesse French <frejes32@gmail.com>
2022-11-13 12:00:00 -0600

    add kata

---
+++ 8kyu/grasshopper-debug-say-hello.js
@@ -0,0 +1,12 @@
+// Grasshopper - Debug sayHello
+
+// DESCRIPTION:
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// EXAMPLE: 
+// Output: Hello, Mr. Spock
+
+// SOLUTION:
+function sayHello(name){
+    return `Hello, ${name}`
+}
