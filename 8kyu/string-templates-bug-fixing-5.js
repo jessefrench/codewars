f682446b78aca7d0dfd5cbbce8498f74428a444a
Jesse French <frejes32@gmail.com>
2022-12-15 12:00:00 -0600

    add kata

---
+++ 8kyu/string-templates-bug-fixing-5.js
@@ -0,0 +1,9 @@
+// String Templates - Bug Fixing #5
+
+// DESCRIPTION:
+// Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+
+// SOLUTION:
+function buildString(...template){
+    return `I like ${template.join(', ')}!`
+}
