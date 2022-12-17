66d218feec89ce8397cb314277e014d201f463b0
Jesse French <frejes32@gmail.com>
2022-12-17 14:38:06 -0600

    add kata

---
+++ 8kyu/template-strings.js
@@ -0,0 +1,18 @@
+// Template Strings
+
+// DESCRIPTION:
+// This kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+
+// TASK:
+// Your task is to return the correct string using the Template String Feature
+
+// INPUT:
+// Two Strings, no validation is needed
+
+// OUTPUT:
+// You must output a string containing the two strings with the word ```' are '```
+
+// SOLUTION:
+var TempleStrings = function(obj, feature){
+    return `${obj} are ${feature}`
+}
