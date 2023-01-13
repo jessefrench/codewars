1c3eb13f752b2dbf5d661932e1044d476dd6efbb
Jesse French <frejes32@gmail.com>
2023-01-13 17:29:49 -0600

    add kata

---
+++ 8kyu/grasshopper-terminal-game-move-function.js
@@ -0,0 +1,15 @@
+// Grasshopper - Terminal Game Move Function
+
+// DESCRIPTION:
+// In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+// TASK:
+// Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+// EXAMPLE:
+// move(3, 6) should equal 15
+
+// SOLUTION:
+function move(position, roll){
+    return position + roll * 2
+}
