709aae0939e7c3094ecb5fd7de11c1973bc50a01
Jesse French <frejes32@gmail.com>
2023-01-15 12:00:00 -0600

    add kata

---
+++ 7kyu/sort-numbers.js
@@ -0,0 +1,13 @@
+// Sort Numbers
+
+// DESCRIPTION:
+// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+// EXAMPLES:
+// solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+// solution(null); // should return []
+
+// SOLUTION:
+function solution(nums){
+    return nums === null ? [] : nums.sort((a,b) => a - b)
+}
