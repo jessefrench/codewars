1e24ff0837d0e6840888780524cafbb4b6ab2c84
Jesse French <frejes32@gmail.com>
2023-01-22 12:00:00 -0600

    add kata

---
+++ 7kyu/the-coupon-code.js
@@ -0,0 +1,16 @@
+// The Coupon Code
+
+// DESCRIPTION:
+// Your online store likes to give out coupons for special occasions. Some customers try to cheat the system by entering invalid codes or using expired coupons.
+
+// TASK:
+// Write a function called checkCoupon which verifies that a coupon code is valid and not expired. A coupon is no more valid on the day AFTER the expiration date. All dates will be passed as strings in this format: "MONTH DATE, YEAR".
+
+// EXAMPLES:
+// checkCoupon("123", "123", "July 9, 2015", "July 9, 2015")  ===  true
+// checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
+
+// SOLUTION:
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+    return enteredCode === correctCode && Date.parse(currentDate) <= Date.parse(expirationDate)
+}
