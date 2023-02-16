e2d5691cff763eb0b8f7ca089005ea8e27735ac0
Jesse French <frejes32@gmail.com>
2023-02-16 12:00:00 -0600

    add kata

---
+++ 8kyu/not-so-black-box.js
@@ -0,0 +1,8 @@
+// Not So Black Box
+
+// DESCRIPTION:
+// The kata has been preloaded with a function called blackBox. Figure out what the function does. You may need to think laterally!
+
+// SOLUTION:
+console.log(blackBox.toString())
+JSopenSesame()
