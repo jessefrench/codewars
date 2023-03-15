f2ff5a971331f22e1cee561b15b0b07279915f81
Jesse French <frejes32@gmail.com>
2023-03-15 12:00:00 -0500

    add kata

---
+++ 8kyu/bin-to-decimal.js
@@ -0,0 +1,9 @@
+// Bin to Decimal
+
+// DESCRIPTION:
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+// SOLUTION:
+function binToDec(bin){
+    return parseInt(bin, 2)
+}
