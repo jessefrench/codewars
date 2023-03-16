eb76ad026b7b3c58a0ee078ff3a54cec0a16b7ba
Jesse French <frejes32@gmail.com>
2023-03-16 12:00:00 -0500

    add kata

---
+++ 8kyu/multiple-of-index.js
@@ -0,0 +1,14 @@
+// Multiple of Index
+
+// DESCRIPTION:
+// Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+
+// EXAMPLES:
+// [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
+// [68, -1, 1, -7, 10, 10] => [-1, 10]
+// [-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]
+
+// SOLUTION:
+function multipleOfIndex(arr){
+    return arr.filter((e,i) => e % i === 0)
+}
