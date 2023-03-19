198721627b0e601da7f91f4d9d162aec7a8186dd
Jesse French <frejes32@gmail.com>
2023-03-19 12:00:00 -0500

    add kata

---
+++ 8kyu/hex-to-decimal.js
@@ -0,0 +1,9 @@
+// Hex to Decimal
+
+// DESCRIPTION:
+// Complete the function which converts a hex number (given as a string) to a decimal number.
+
+// SOLUTION:
+function hexToDec(hexString){
+    return parseInt(hexString, 16)
+}
