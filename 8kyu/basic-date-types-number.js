3269caaf6b55c0ca3d2731549e032d21182b9189
Jesse French <frejes32@gmail.com>
2023-04-11 12:00:00 -0500

    add kata

---
+++ 8kyu/basic-date-types-number.js
@@ -0,0 +1,21 @@
+// Training JS #2: Basic data types--Number
+
+// DESCRIPTION:
+// In JavaScript, Number is one of basic data types. It can be positive: 1, 2, 3; negative: -1, -100; integer: 123, 456; or decimal: 3.1415926, -8.88 etc. Numbers can use operators such as + - * / %
+
+// TASK:
+// I've written five functions: equal1, equal2, equal3, equal4, and equal5, and defined six global variables: v1, v2, v3, v4, v5, and v6. Every function has two local variables a and b. Please set the appropriate value for the two variables (select from v1--v6) making these functions return value equal to 100. The function equal1 is completed, please refer to this example to complete the following functions.
+
+// SOLUTION:
+let v1 = 50,
+    v2 = 100,
+    v3 = 150,
+    v4 = 200,
+    v5 = 2,
+    v6 = 250
+
+const equal1 = () => v1 + v1
+const equal2 = () => v3 - v1
+const equal3 = () => v1 * v5
+const equal4 = () => v4 / v5
+const equal5 = () => v2 % v4
